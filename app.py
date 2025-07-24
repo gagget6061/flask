6b4b14807b07426e3c4deb99880774ce9d695280
gagget6061 <gagget6061@gmail.com>
2025-07-25 02:48:57 +0900

    new5web

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
 @app.route("/")
 def index():
-    return send_from_directory('html2',"index.html")
+    return render_template('./index.html')
 
-@app.route('/<path:name>')
-def start(name):
-    return send_from_directory('html2',name)
+@app.route("/products")
+def index():
+    return render_template('./products.html')
+
+#@app.route("/")
+#def index():
+ #   return send_from_directory('html2',"index.html")
+
+#@app.route('/<path:name>')
+#def start(name):
+#    return send_from_directory('html2',name)
 
 #if __name__ == "__main__":
 #    app.run(host="0.0.0.0", debug=True, port=5000)
