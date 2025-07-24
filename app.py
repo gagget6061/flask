7c1d6736117cde6f847f6271bf1c815bcf3cc3b0
gagget6061 <gagget6061@gmail.com>
2025-07-24 23:42:42 +0900

    new web

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+@app.route("/hello")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
 @app.route("/")
 def index():
-    return render_template('./index.html')
+    return send_from_directory('templates',"index.html")
+
+@app.route('/<path:name>')
+def start(name):
+    return send_from_directory('templates',name)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=5000)
